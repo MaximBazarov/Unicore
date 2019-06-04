fbaad0d4cf04bfd0ad549fd37ac950a10bf34e20
Maxim Bazarov <maxim.bazarov@zalando.de>
2019-06-04 16:40:17 +0200

    removed spm

--- Package.swift
+++ Package.swift
@@ -11,16 +11,17 @@ let package = Package(
             name: "Unicore",
             targets: ["Unicore"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+    dependencies: [        
+        .package(url: "https://github.com/Unicore/Command.git", .branch("spm")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Unicore",
-            dependencies: []),
+            dependencies: [
+                "Command",
+            ]),
         .testTarget(
             name: "UnicoreTests",
             dependencies: ["Unicore"]),
