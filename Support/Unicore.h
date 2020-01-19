a96e5c3a3c2683a14d2d1b745ad13959fec77dc3
Maxim Bazarov <bazaroffma@gmail.com>
2020-01-19 10:53:28 +0100

    add carthage support

--- Support/Unicore.h
+++ Support/Unicore.h
@@ -1,4 +1,4 @@
 @import Foundation;
 
-FOUNDATION_EXPORT double UnicoreVersionNumber;
-FOUNDATION_EXPORT const unsigned char UnicoreFoundationVersionString[];
+FOUNDATION_EXPORT double CommandVersionNumber;
+FOUNDATION_EXPORT const unsigned char CommandVersionString[];
